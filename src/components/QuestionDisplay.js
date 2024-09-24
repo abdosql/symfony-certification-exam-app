3b110db92c3fe7ec352f11157b008c28094039ba
abdosql <abdosql@users.noreply.github.com>
2024-09-25 00:44:07 +0200

    fix exam

--- src/components/QuestionDisplay.js
+++ src/components/QuestionDisplay.js
@@ -1,52 +1,64 @@
 import React from 'react';
 
 function QuestionDisplay({ question, userAnswer, onAnswer }) {
-  const handleAnswerChange = (event) => {
-    const answer = event.target.value;
-    console.log(`Answer selected for question ${question.id}:`, answer);
+  const handleChange = (event) => {
+    let answer;
+    switch (question.type) {
+      case 'true_false':
+        answer = event.target.value === 'true';
+        break;
+      case 'single':
+        answer = event.target.value;
+        break;
+      case 'multiple':
+        const selectedOptions = Array.from(event.target.selectedOptions, option => option.value);
+        answer = selectedOptions;
+        break;
+      default:
+        answer = event.target.value;
+    }
     onAnswer(question.id, answer);
   };
-
-  if (!question || !question.options) {
-    return <div className="error">Question data is not available.</div>;
-  }
 
   return (
     <div className="question-display">
       <h3>{question.text}</h3>
-      {question.type === 'boolean' ? (
+      {question.type === 'true_false' && (
         <div>
           <label>
             <input
               type="radio"
+              name={question.id}
               value="true"
-              checked={userAnswer === 'true'}
-              onChange={handleAnswerChange}
-            />
-            True
+              checked={userAnswer === true}
+              onChange={handleChange}
+            /> True
           </label>
           <label>
             <input
               type="radio"
+              name={question.id}
               value="false"
-              checked={userAnswer === 'false'}
-              onChange={handleAnswerChange}
-            />
-            False
+              checked={userAnswer === false}
+              onChange={handleChange}
+            /> False
           </label>
         </div>
-      ) : (
-        question.options.map((option) => (
-          <label key={option}>
-            <input
-              type={question.type === 'single' ? 'radio' : 'checkbox'}
-              value={option}
-              checked={question.type === 'single' ? userAnswer === option : userAnswer && userAnswer.includes(option)}
-              onChange={handleAnswerChange}
-            />
-            {option}
-          </label>
-        ))
+      )}
+      {question.type === 'single' && (
+        <select value={userAnswer || ''} onChange={handleChange}>
+          <option value="">Select an answer</option>
+          {question.options.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
+          ))}
+        </select>
+      )}
+      {question.type === 'multiple' && (
+        <select multiple value={userAnswer || []} onChange={handleChange}>
+          {question.options.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
+          ))}
+        </select>
       )}
     </div>
   );
